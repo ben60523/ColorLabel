e8b622191dd8bcb274b26cfe5202e5ac3a63cf62
yklu0330 <yklu0330@gmail.com>
2021-07-14 17:08:14 +0800

    Fix contour filling bugs & remove noise

--- label.py
+++ label.py
@@ -1,55 +1,54 @@
 import cv2
 import numpy as np
-import colorList
+import collections
 import os
 from tqdm import tqdm
 import argparse
 
 
-check_ll = [-2, -1, 0, 1, 2]
-def get_color(frame):
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    color_dict = colorList.getColorList()
-    yellow_point_num = 0
-    green_point_num = 0
-    yellow_point_list = []
-    green_point_list = []
-    for color_which, d in enumerate(color_dict):
-        if color_which == 0:
-            yellow_mask = cv2.inRange(hsv,color_dict[d][0], color_dict[d][1])
-            for i in range(yellow_mask.shape[0]):
-                for j in range(yellow_mask.shape[1]):
-                    if yellow_mask[i][j] == 255:
-                        check_num = 0
-                        for x in check_ll:
-                            for y in check_ll:
-                                if yellow_mask[i+x][j+y] == 255:
-                                    check_num += 1
-                        if check_num >= 10:        
-                            yellow_point_num += 1
-                            yellow_point_list.append([j,i])
-                        else:
-                            yellow_mask[i][j] = 0
-        if color_which == 1:
-            green_mask = cv2.inRange(hsv, color_dict[d][0], color_dict[d][1])
-            for i in range(green_mask.shape[0]):
-                for j in range(green_mask.shape[1]):
-                    if green_mask[i][j] == 255:
-                        
-                        check_num = 0
-                        for x in check_ll:
-                            for y in check_ll:
-                                if green_mask[i + x][j + y] == 255:
-                                    check_num += 1
-                        if check_num >= 10:
-                            green_point_num += 1
-                            green_point_list.append([j, i])
-                        else:
-                            green_mask[i][j] = 0
-    if yellow_point_num >= green_point_num:
-        return yellow_mask, np.array(yellow_point_list)
-    else:
-        return green_mask, np.array(green_point_list)
+def getColorList():
+    dict = collections.defaultdict(list)
+    #yellow
+    # lower_yellow = np.array([26, 43, 46])
+    lower_yellow = np.array([23, 43, 46])
+    upper_yellow = np.array([34, 255, 255])
+    color_list = []
+    color_list.append(lower_yellow)
+    color_list.append(upper_yellow)
+    dict['yellow'] = color_list
+    #green
+    lower_green = np.array([35, 43, 46])
+    upper_green = np.array([77, 255, 255])
+    color_list = []
+    color_list.append(lower_green)
+    color_list.append(upper_green)
+    dict['green'] = color_list
+    return dict
+
+def fill_contours(arr):
+    return np.maximum.accumulate(arr, 1) &\
+           np.maximum.accumulate(arr[:, ::-1], 1)[:, ::-1] &\
+           np.maximum.accumulate(arr[::-1, :], 0)[::-1, :] &\
+           np.maximum.accumulate(arr, 0)
+
+def mask_generate(img):
+    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    
+    # find green contour
+    lower_green = np.array([35, 43, 46])
+    upper_green = np.array([77, 255, 255])
+    green_mask = cv2.inRange(hsv, lower_green, upper_green)
+    bool_mask = np.where((green_mask == 255), 1, 0).astype(int)
+    
+    # fill green contour
+    fill_mask = fill_contours(bool_mask) * 255
+    
+    # remove noise
+    kernel = np.ones((7, 7), np.uint8)
+    fill_mask = fill_mask.astype(np.uint8)
+    output_mask = cv2.morphologyEx(fill_mask, cv2.MORPH_OPEN, kernel)
+
+    return output_mask
 
 def LISTDIR(path):
     out = os.listdir(path)
@@ -62,15 +61,13 @@ if __name__ == '__main__':
     parser.add_argument("--output_path")
     args = parser.parse_args()
     os.makedirs(args.output_path, exist_ok=True)
-    for num_files in tqdm(LISTDIR(args.input_path)):
-        full_path = os.path.join(args.input_path, num_files)
-        out_file_path = os.path.join(args.output_path, num_files)
+    for file_name in tqdm(LISTDIR(args.input_path)):
+        full_path = os.path.join(args.input_path, file_name)
+        out_file_path = os.path.join(args.output_path, file_name)
+        file_name = file_name.split(".")[0]
 
         if os.path.isfile(full_path):
-            frame = cv2.imread(full_path)
-            gs_img, point_list = get_color(frame)
-            if point_list.size == 0:
-                cv2.imwrite(out_file_path, frame)
-            else:
-                cv2.fillPoly(gs_img, [point_list], 255) 
-                cv2.imwrite(out_file_path, gs_img)                  
+            img = cv2.imread(full_path)
+            mask = mask_generate(img)
+            cv2.imwrite(out_file_path, mask)
+            
