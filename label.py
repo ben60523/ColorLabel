814d0ed74f4fbe1f6895e310f931b767628a3bf9
ssyang <ben60523@gmail.com>
2021-07-16 10:58:59 +0800

    Fix bug about fill contours

--- label.py
+++ label.py
@@ -12,6 +12,79 @@ def fill_contours(arr):
            np.maximum.accumulate(arr[::-1, :], 0)[::-1, :] &\
            np.maximum.accumulate(arr, 0)
 
+
+def fill_contours_2(arr):
+    output_forward_horizontal = np.zeros(arr.shape)
+    output_forward_vertical = np.zeros(arr.shape)
+    output_backward_horizontal = np.zeros(arr.shape)
+    output_backward_vertical = np.zeros(arr.shape)
+    
+    h, w = arr.shape
+    
+    # horizontal scanning
+    for y in range(h):
+        start_point_forward = -1
+        stop_point_forward = -1
+        start_point_backward = -1
+        stop_point_backward = -1
+
+        for x in range(w):
+            # from left to right
+            if arr[y, x- 1] > 0 and arr[y, x] == 0:
+                if start_point_forward < 0:
+                    start_point_forward = x - 1
+                else:
+                    stop_point_forward = x - 1
+            if start_point_forward > 0 and stop_point_forward > 0:
+                output_forward_horizontal[y, start_point_forward:stop_point_forward] = 1
+                start_point_forward = stop_point_forward = -1
+
+            # from right to left
+            x_backward = w - x - 1
+            if arr[y, x_backward] == 0 and arr[y, x_backward - 1] > 0:
+                if stop_point_backward < 0:
+                    stop_point_backward = x_backward
+                else:
+                    start_point_backward = x_backward
+            if start_point_backward > 0 and stop_point_backward > 0:
+                output_backward_horizontal[y, start_point_backward:stop_point_backward] = 1
+                start_point_backward = stop_point_backward = -1
+
+    output_horizontal = np.logical_or(output_forward_horizontal, output_backward_horizontal)
+    # vertical scanning
+    for x in range(w):
+        start_point_forward = -1
+        stop_point_forward = -1
+        start_point_backward = -1
+        stop_point_backward = -1
+        
+        for y in range(h):
+            # from top to bottom
+            if arr[y - 1, x] > 0 and arr[y, x] == 0:
+                if start_point_forward < 0:
+                    start_point_forward = y - 1
+                else:
+                    stop_point_forward = y - 1
+            if start_point_forward > 0 and stop_point_forward > 0:
+                output_forward_vertical[start_point_forward:stop_point_forward, x] = 1
+                start_point_forward = stop_point_forward = -1
+            
+            # from bottom to top
+            y_backward = h - y - 1
+            if arr[y_backward - 1, x] == 0 and arr[y_backward, x] > 0:
+                if stop_point_backward < 0:
+                    stop_point_backward = y_backward
+                else:
+                    start_point_backward = y_backward
+
+            if start_point_backward > 0 and stop_point_backward > 0:
+                output_backward_vertical[start_point_backward:stop_point_backward, x] = 1
+                start_point_backward = stop_point_backward = -1
+                
+    output_vertical = np.logical_or(output_forward_vertical, output_backward_vertical)
+
+    return np.logical_and(output_horizontal, output_vertical)
+
 def mask_generate(img):
     hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     
@@ -22,12 +95,12 @@ def mask_generate(img):
     bool_mask = np.where((green_mask == 255), 1, 0).astype(int)
     
     # fill green contour
-    fill_mask = fill_contours(bool_mask) * 255
-    
+    fill_mask = fill_contours_2(bool_mask) * 255
     # remove noise
-    kernel = np.ones((7, 7), np.uint8)
+    kernel = np.ones((9, 9), np.uint8)
     fill_mask = fill_mask.astype(np.uint8)
     output_mask = cv2.morphologyEx(fill_mask, cv2.MORPH_OPEN, kernel)
+    output_mask = cv2.dilate(output_mask, kernel)
 
     return output_mask
 
